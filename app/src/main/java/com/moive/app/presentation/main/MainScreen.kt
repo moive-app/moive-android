db9dd5723e580d073273b2c5fd5fcdd3b2552191
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-01 03:24:49 +0900

    [feat/#11] Toast 구현

--- app/src/main/java/com/moive/app/presentation/main/MainScreen.kt
+++ app/src/main/java/com/moive/app/presentation/main/MainScreen.kt
@@ -1,15 +1,33 @@
 package com.moive.app.presentation.main
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.moive.app.core.designsystem.component.toast.LocalToastTrigger
+import com.moive.app.core.designsystem.component.toast.MoiveToast
 import com.moive.app.presentation.main.component.MainBottomBar
 import com.moive.app.presentation.main.component.MainTab
 import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private const val TOAST_DURATION = 3000L
 
 @Composable
 fun MainScreen(
@@ -18,22 +36,72 @@ fun MainScreen(
     val currentTab by appState.currentTab.collectAsStateWithLifecycle()
     val isBottomBarVisible by appState.isBottomBarVisible.collectAsStateWithLifecycle()
 
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        bottomBar = {
-            MainBottomBar(
-                isVisible = isBottomBarVisible,
-                tabs = MainTab.entries.toPersistentList(),
-                currentTab = currentTab,
-                onTabSelected = appState::navigate,
-                modifier = Modifier.navigationBarsPadding()
-            )
-        },
-    ) { innerPadding ->
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
 
-        MainNavHost(
-            appState = appState,
-            innerPadding = innerPadding,
-        )
+    var job by remember { mutableStateOf<Job?>(null) }
+    var toastDisplayBottomInset by remember { mutableStateOf(0.dp) }
+
+    val onShowToast: (String) -> Unit = { message ->
+        job?.cancel()
+        job = coroutineScope.launch {
+            snackbarHostState.currentSnackbarData?.dismiss()
+
+            launch {
+                delay(TOAST_DURATION)
+                snackbarHostState.currentSnackbarData?.dismiss()
+            }
+
+            snackbarHostState.showSnackbar(
+                message = message,
+                withDismissAction = false,
+            )
+        }
+    }
+
+    CompositionLocalProvider(
+        LocalToastTrigger provides onShowToast,
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                bottomBar = {
+                    MainBottomBar(
+                        isVisible = isBottomBarVisible,
+                        tabs = MainTab.entries.toPersistentList(),
+                        currentTab = currentTab,
+                        onTabSelected = appState::navigate,
+                        modifier = Modifier.navigationBarsPadding()
+                    )
+                },
+            ) { innerPadding ->
+
+                MainNavHost(
+                    appState = appState,
+                    innerPadding = innerPadding,
+                )
+            }
+
+            SnackbarHost(
+                hostState = snackbarHostState,
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .navigationBarsPadding(),
+            ) { data ->
+
+                MoiveToast(
+                    text = data.visuals.message,
+                    modifier = Modifier
+                        .padding(horizontal = 20.dp, vertical = 20.dp)
+                        .padding(
+                            bottom = if (toastDisplayBottomInset > 20.dp) {
+                                toastDisplayBottomInset
+                            } else {
+                                0.dp
+                            },
+                        ),
+                )
+            }
+        }
     }
 }
