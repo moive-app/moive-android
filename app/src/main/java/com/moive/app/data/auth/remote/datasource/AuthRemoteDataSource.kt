bc49a80c2695b1f81ef69ead9a854c703bb515a0
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-01 02:17:53 +0900

    [feat/#8] authDataSource 추가 - 회원가입

--- app/src/main/java/com/moive/app/data/auth/remote/datasource/AuthRemoteDataSource.kt
+++ app/src/main/java/com/moive/app/data/auth/remote/datasource/AuthRemoteDataSource.kt
@@ -1,8 +1,16 @@
 package com.moive.app.data.auth.remote.datasource
 
 import com.moive.app.data.auth.remote.dto.KakaoLoginResponse
+import com.moive.app.data.auth.remote.dto.SignUpResponse
 import com.moive.app.data.common.dto.BaseResponse
 
 interface AuthRemoteDataSource {
     suspend fun postKakaoLogin(accessToken: String): BaseResponse<KakaoLoginResponse>
+
+    suspend fun postSignUp(
+        accessToken: String,
+        isServiceAgreed: Boolean,
+        isPrivacyAgreed: Boolean,
+        isMarketingAgreed: Boolean,
+    ): BaseResponse<SignUpResponse>
 }
