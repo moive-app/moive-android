59223cef1bb8df4fa04dcb12b333ba868a54ce88
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-08-31 19:50:05 +0900

    [feat/#3] model - immutable 어노테이션 추가

--- app/src/main/java/com/moive/app/data/auth/model/KakaoLoginModel.kt
+++ app/src/main/java/com/moive/app/data/auth/model/KakaoLoginModel.kt
@@ -1,5 +1,8 @@
 package com.moive.app.data.auth.model
 
+import androidx.compose.runtime.Immutable
+
+@Immutable
 data class KakaoLoginModel(
     val registered: Boolean,
     val nickname: String,
