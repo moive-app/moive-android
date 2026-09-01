ed97d10cf3153f44a4fec43c3466b822ff89de72
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-01 21:43:03 +0900

    [feat/#22] Meeting 화면 추가 및 네비 연결

---
+++ app/src/main/java/com/moive/app/presentation/meeting/navigation/MeetingNavigation.kt
@@ -0,0 +1,29 @@
+package com.moive.app.presentation.meeting.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.moive.app.core.navigation.Route
+import com.moive.app.presentation.home.HomeRoute
+import kotlinx.serialization.Serializable
+
+fun NavController.navigateToMeeting(
+    navOptions: NavOptions? = null
+) = navigate(Meeting, navOptions)
+
+fun NavGraphBuilder.meetingGraph(
+    innerPadding: PaddingValues,
+) {
+    composable<Meeting> {
+        HomeRoute(
+            modifier = Modifier.padding(innerPadding),
+        )
+    }
+}
+
+@Serializable
+data object Meeting: Route
