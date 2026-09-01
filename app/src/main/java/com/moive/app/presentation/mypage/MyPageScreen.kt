1431ec161a5c1c2d2481f0585d8e23bd8f45ff82
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-01 14:39:29 +0900

    [ui/#13] 로그아웃 임시 ui 추가
    - 추후 수정 예정

--- app/src/main/java/com/moive/app/presentation/mypage/MyPageScreen.kt
+++ app/src/main/java/com/moive/app/presentation/mypage/MyPageScreen.kt
@@ -1,32 +1,43 @@
 package com.moive.app.presentation.mypage
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.moive.app.core.designsystem.theme.MoiveTheme
+import com.moive.app.core.extensions.noRippleClickable
 
 @Composable
 fun MyPageRoute(
     modifier: Modifier = Modifier,
 ) {
     MyPageScreen(
+        onLogoutClick = {},
         modifier = modifier,
     )
 }
 
 @Composable
 private fun MyPageScreen(
+    onLogoutClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Box(
+    Column(
         modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         Text(text = "MyPage Screen")
+        Text(
+            text = "로그아웃",
+            modifier = Modifier.noRippleClickable(onClick = onLogoutClick),
+        )
     }
 }
 
@@ -35,6 +46,8 @@ private fun MyPageScreen(
 @Composable
 private fun MyPageScreenPreview() {
     MoiveTheme {
-        MyPageScreen()
+        MyPageScreen(
+            onLogoutClick = {},
+        )
     }
 }
