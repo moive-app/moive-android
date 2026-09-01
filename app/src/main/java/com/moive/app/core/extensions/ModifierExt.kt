490f6827960bbb50870f8a88661781ac3339b86e
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-01 19:40:32 +0900

    [ui/#16] 커스텀 shadow 구현

--- app/src/main/java/com/moive/app/core/extensions/ModifierExt.kt
+++ app/src/main/java/com/moive/app/core/extensions/ModifierExt.kt
@@ -1,10 +1,22 @@
 package com.moive.app.core.extensions
 
+import android.graphics.BlurMaskFilter
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.drawOutline
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 
 fun Modifier.noRippleClickable(
     onClick: () -> Unit,
@@ -16,4 +28,48 @@ fun Modifier.noRippleClickable(
         onClick = onClick,
         enabled = isEnabled,
     )
+}
+
+@Composable
+fun Modifier.customShadow(
+    shape: Shape,
+    color: Color = Color.Black,
+    blur: Dp = 0.dp,
+    offsetX: Dp = 0.dp,
+    offsetY: Dp = 0.dp,
+    spread: Dp = 0f.dp,
+) = composed {
+    val localDensity = LocalDensity.current
+    val paint = remember(color, blur) {
+        Paint().apply {
+            this.color = color
+            val blurPx = with(localDensity) { blur.toPx() }
+
+            if (blurPx > 0) {
+                asFrameworkPaint().maskFilter =
+                    BlurMaskFilter(blurPx, BlurMaskFilter.Blur.NORMAL)
+            }
+        }
+    }
+
+    drawBehind {
+        if (size.width == 0f || size.height == 0f) return@drawBehind
+
+        val offsetXPx = offsetX.toPx()
+        val offsetYPx = offsetY.toPx()
+        val spreadPx = spread.toPx()
+
+        val shadowWidth = size.width + spreadPx
+        val shadowHeight = size.height + spreadPx
+
+        val shadowSize = Size(width = shadowWidth, height = shadowHeight)
+        val shadowOutline = shape.createOutline(size = shadowSize, layoutDirection = layoutDirection, density = this)
+
+        drawIntoCanvas { canvas ->
+            canvas.save()
+            canvas.translate(dx = offsetXPx, dy = offsetYPx)
+            canvas.drawOutline(outline = shadowOutline, paint = paint)
+            canvas.restore()
+        }
+    }
 }
