c757e27392f1bca32249fd61f398a37891a1eb06
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-04 02:11:12 +0900

    [feat/#32] 모임 상세 네비 구현

---
+++ app/src/main/java/com/moive/app/presentation/meeting/detail/navigation/MeetingDetailNavigation.kt
@@ -0,0 +1,32 @@
+package com.moive.app.presentation.meeting.detail.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.moive.app.core.navigation.Route
+import com.moive.app.presentation.meeting.confirmed.navigation.navigateToMeetingConfirmed
+import com.moive.app.presentation.meeting.detail.MeetingDetailRoute
+import kotlinx.serialization.Serializable
+
+fun NavController.navigateToMeetingDetail(
+    navOptions: NavOptions? = null
+) = navigate(MeetingDetail, navOptions)
+
+fun NavGraphBuilder.meetingDetailGraph(
+    navController: NavController,
+    innerPadding: PaddingValues,
+) {
+    composable<MeetingDetail> {
+        MeetingDetailRoute(
+            navigateToMeetingConfirmed = navController::navigateToMeetingConfirmed,
+            modifier = Modifier.padding(innerPadding),
+        )
+    }
+}
+
+@Serializable
+data object MeetingDetail : Route
