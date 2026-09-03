171025cf8d0a0935fb8d18196e45faa34f5973cb
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-04 01:55:23 +0900

    [ui/#32] 임시 home ui 추가

--- app/src/main/java/com/moive/app/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/moive/app/presentation/home/HomeScreen.kt
@@ -1,13 +1,17 @@
 package com.moive.app.presentation.home
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.moive.app.core.designsystem.theme.MoiveTheme
+import com.moive.app.core.extensions.noRippleClickable
 
 @Composable
 fun HomeRoute(
@@ -20,21 +24,48 @@ fun HomeRoute(
 
 @Composable
 private fun HomeScreen(
+    onShowListClick: () -> Unit,
+    onOngoingMeetingClick: () -> Unit,
+    onEndMeetingClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Box(
-        modifier = modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
     ) {
-        Text(text = "Home Screen")
+        Text(
+            text = "내모임 전체보기",
+            modifier = Modifier.noRippleClickable(onClick = onShowListClick),
+        )
+
+        Text(
+            text = "진행중인 모임",
+            modifier = Modifier
+                .fillMaxWidth()
+                .noRippleClickable(onClick = onOngoingMeetingClick)
+                .padding(16.dp),
+        )
+
+        Text(
+            text = "종료된 모임",
+            modifier = Modifier
+                .fillMaxWidth()
+                .noRippleClickable(onClick = onEndMeetingClick)
+                .padding(16.dp),
+        )
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
 private fun HomeScreenPreview() {
     MoiveTheme {
-        HomeScreen()
+        HomeScreen(
+            onShowListClick = {},
+            onOngoingMeetingClick = {},
+            onEndMeetingClick = {},
+        )
     }
 }
