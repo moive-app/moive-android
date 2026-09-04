ad4c6981df73f1dc23852dd5c4ee71143084b53b
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-05 00:32:25 +0900

    [feat/#4] 카카오맵 그리기 - rememberMapViewWithLifecycle 구현
    - 생명주기에 따른 맵 실행을 위함

---
+++ app/src/main/java/com/moive/app/presentation/voting/component/rememberMapViewWithLifecycle.kt
@@ -0,0 +1,74 @@
+package com.moive.app.presentation.voting.component
+
+import android.view.View
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.LatLng
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
+import com.kakao.vectormap.camera.CameraUpdateFactory
+import timber.log.Timber
+
+@Composable
+fun rememberMapViewWithLifecycle(
+    locationX: Double,
+    locationY: Double,
+    onMapReady: (KakaoMap) -> Unit,
+): View {
+    val context = LocalContext.current
+    val mapView = remember { MapView(context) }
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+
+    DisposableEffect(lifecycle) {
+        val observer = object : DefaultLifecycleObserver {
+            override fun onCreate(owner: LifecycleOwner) {
+                mapView.start(
+                    object : MapLifeCycleCallback() {
+                        override fun onMapDestroy() {
+                            Timber.tag("KakaoMap").d("카카오 맵이 정상적으로 종료됐습니다.")
+                        }
+
+                        override fun onMapError(error: Exception) {
+                            Timber.tag("KakaoMap").e("카카오 맵 사용 중 에러가 발생했습니다. $error")
+                        }
+                    },
+                    object : KakaoMapReadyCallback() {
+                        override fun getPosition(): LatLng {
+                            return LatLng.from(locationY, locationX)
+                        }
+
+                        override fun onMapReady(kakaoMap: KakaoMap) {
+                            val cameraUpdate = CameraUpdateFactory.newCenterPosition(LatLng.from(locationY, locationX), 13)
+                            kakaoMap.moveCamera(cameraUpdate)
+
+                            onMapReady(kakaoMap)
+                        }
+                    }
+                )
+            }
+
+            override fun onResume(owner: LifecycleOwner) {
+                mapView.resume()
+            }
+
+            override fun onPause(owner: LifecycleOwner) {
+                mapView.pause()
+            }
+
+        }
+
+        lifecycle.addObserver(observer)
+        onDispose {
+            lifecycle.removeObserver(observer)
+        }
+    }
+    return mapView
+}
+
