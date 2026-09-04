abf41710e3355bcfda9fcaf8946222969ae289ea
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-05 03:25:40 +0900

    [ui/#38] toast message ui 구현

--- app/src/main/java/com/moive/app/core/designsystem/component/toast/MoiveToast.kt
+++ app/src/main/java/com/moive/app/core/designsystem/component/toast/MoiveToast.kt
@@ -1,38 +1,61 @@
 package com.moive.app.core.designsystem.component.toast
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.moive.app.R
 import com.moive.app.core.designsystem.theme.MoiveTheme
+import com.moive.app.core.designsystem.theme.MoiveTheme.colors
+import com.moive.app.core.designsystem.theme.MoiveTheme.radius
+import com.moive.app.core.designsystem.theme.MoiveTheme.typography
 
 @Composable
 fun MoiveToast(
     text: String,
+    type: ToastType,
     modifier: Modifier = Modifier,
 ) {
+    val img = when(type) {
+        ToastType.ERROR -> R.drawable.img_toast_error
+        ToastType.SUCCESS -> R.drawable.img_toast_success
+        ToastType.CAUTION -> R.drawable.img_toast_caution
+    }
+
     Row(
         modifier = modifier
             .fillMaxWidth()
             .background(
-                color = MoiveTheme.colors.fill.default02,
-                shape = RoundedCornerShape(6.dp),
+                color = colors.fill.default02,
+                shape = RoundedCornerShape(radius.md),
             )
-            .padding(horizontal = 4.dp),
+            .padding(horizontal = 16.dp, vertical = 12.dp),
         verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
     ) {
+
+        Image(
+            painter = painterResource(img),
+            contentDescription = null,
+            modifier = Modifier.size(22.dp),
+        )
 
         Text(
             text = text,
-            color = MoiveTheme.colors.text.onBg,
-            modifier = Modifier.padding(vertical = 11.dp)
+            color = colors.text.onBg,
+            style = typography.label.smM,
         )
 
     }
@@ -41,9 +64,17 @@ fun MoiveToast(
 @Preview
 @Composable
 private fun MoiveToastPreview() {
-    MoiveTheme{
-        MoiveToast(
-            text = "토스트 메세지입니다.",
-        )
+    MoiveTheme {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            ToastType.entries.forEach { type ->
+                MoiveToast(
+                    text = "${type.name} 토스트 메세지입니다.",
+                    type = type,
+                )
+            }
+        }
     }
 }
