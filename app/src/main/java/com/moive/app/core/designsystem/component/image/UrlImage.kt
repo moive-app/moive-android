723d23e75e0e0138b13a029d670e0224b0770620
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-06 18:18:06 +0900

    [mod/#60] 기본 이미지 임시 추가

--- app/src/main/java/com/moive/app/core/designsystem/component/image/UrlImage.kt
+++ app/src/main/java/com/moive/app/core/designsystem/component/image/UrlImage.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,6 +33,8 @@ fun UrlImage(
             model = url,
             contentDescription = contentDescription,
             contentScale = contentScale,
+            placeholder = painterResource(R.drawable.ic_launcher_background),
+            error = painterResource(R.drawable.ic_launcher_background),
             modifier = modifier
         )
     }
