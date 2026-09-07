a2db562d7f4a6e9655bd4fe3ab7ab4e37ee5f944
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-07 14:02:26 +0900

    [del/#66] 미사용 함수 제거

--- app/src/main/java/com/moive/app/presentation/meeting/detail/MeetingDetailViewModel.kt
+++ app/src/main/java/com/moive/app/presentation/meeting/detail/MeetingDetailViewModel.kt
@@ -1,11 +1,9 @@
 package com.moive.app.presentation.meeting.detail
 
 import androidx.lifecycle.ViewModel
-import com.moive.app.presentation.meeting.detail.MeetingDetailContract.MeetingStatus
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,14 +14,4 @@ class MeetingDetailViewModel @Inject constructor(
     private val _uiState = MutableStateFlow(MeetingDetailContract.State())
     val uiState = _uiState.asStateFlow()
 
-    fun changeStatus() {
-        _uiState.update { state ->
-            val nextStatus = when (state.status) {
-                MeetingStatus.INPUTTING -> MeetingStatus.VOTING
-                MeetingStatus.VOTING -> MeetingStatus.CONFIRMED
-                MeetingStatus.CONFIRMED -> MeetingStatus.CONFIRMED
-            }
-            state.copy(status = nextStatus)
-        }
-    }
 }
