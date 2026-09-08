a330ccf95b5cc29c1c98fef323d1cab4bb3d53f7
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-09 00:06:00 +0900

    [mod/#72] SubTitle탑바 높이 및 배경색 수정

--- app/src/main/java/com/moive/app/core/designsystem/component/topbar/MoiveSubTitleTopBar.kt
+++ app/src/main/java/com/moive/app/core/designsystem/component/topbar/MoiveSubTitleTopBar.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
@@ -12,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,15 +27,15 @@ fun MoiveSubTitleTopBar(
     title: String,
     onBackClick: () -> Unit,
     modifier: Modifier = Modifier,
+    backgroundColor: Color = colors.fill.default08,
 ) {
     Row(
         modifier = modifier
             .fillMaxWidth()
             .background(
-                color = colors.fill.default08
+                color = backgroundColor
             )
-            .padding(horizontal = 20.dp, vertical = 15.dp)
-            .height(56.dp),
+            .padding(horizontal = 20.dp, vertical = 15.dp),
         verticalAlignment = Alignment.CenterVertically,
     ) {
         Icon(
