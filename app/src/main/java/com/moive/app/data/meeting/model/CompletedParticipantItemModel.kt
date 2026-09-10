ef387dccf0ac3c50508e39f0b762c4eb7e474f41
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-11 01:49:44 +0900

    [ui/#81] 참여자 리스트 카드 구현

---
+++ app/src/main/java/com/moive/app/data/meeting/model/CompletedParticipantItemModel.kt
@@ -0,0 +1,8 @@
+package com.moive.app.data.meeting.model
+
+data class CompletedParticipantItemModel(
+    val id: Long,
+    val name: String,
+    val profileImageUrl: String,
+    val address: String,
+)
