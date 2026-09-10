fa599af1a9b74b9ea15f0e27e9827772111dcf43
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-10 20:36:05 +0900

    [feat/#84] 회원 탈퇴 api 연동

--- app/src/main/java/com/moive/app/presentation/withdraw/WithDrawViewModel.kt
+++ app/src/main/java/com/moive/app/presentation/withdraw/WithDrawViewModel.kt
@@ -2,6 +2,7 @@ package com.moive.app.presentation.withdraw
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.moive.app.data.user.repository.UserRepository
 import com.moive.app.presentation.withdraw.WithDrawContract.SideEffect
 import com.moive.app.presentation.withdraw.WithDrawContract.SideEffect.NavigateToLogin
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,10 +12,12 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class WithDrawViewModel @Inject constructor(
+    private val userRepository: UserRepository,
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(WithDrawContract.State())
@@ -41,7 +44,23 @@ class WithDrawViewModel @Inject constructor(
     }
 
     fun postWithDraw() = viewModelScope.launch {
-        _sideEffect.send(NavigateToLogin)
+        _uiState.update { it.copy(withDrawUiState = WithDrawUiState.Loading) }
+
+        userRepository.deleteWithdraw()
+            .onSuccess {
+                _uiState.update { it.copy(withDrawUiState = WithDrawUiState.Success) }
+                _sideEffect.send(NavigateToLogin)
+            }
+            .onFailure { error ->
+                Timber.tag(WITH_DRAW_TAG).e(error)
+                _uiState.update {
+                    it.copy(withDrawUiState = WithDrawUiState.Failure(error.message ?: UNKNOWN_ERROR_MESSAGE))
+                }
+            }
     }
 
+    companion object {
+        private const val WITH_DRAW_TAG = "WithDraw"
+        private const val UNKNOWN_ERROR_MESSAGE = "알 수 없는 에러가 발생했습니다."
+    }
 }
