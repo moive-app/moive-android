8be1f8821e8a82a7b47b05bab68021b83e9a48cc
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-10 20:10:06 +0900

    [feat/#84] 내 정보 service, datasource/impl 추가

---
+++ app/src/main/java/com/moive/app/data/user/remote/datasource/UserRemoteDataSource.kt
@@ -0,0 +1,8 @@
+package com.moive.app.data.user.remote.datasource
+
+import com.moive.app.data.common.dto.BaseResponse
+import com.moive.app.data.user.remote.dto.UserResponse
+
+interface UserRemoteDataSource {
+    suspend fun getMyInfo(): BaseResponse<UserResponse>
+}
