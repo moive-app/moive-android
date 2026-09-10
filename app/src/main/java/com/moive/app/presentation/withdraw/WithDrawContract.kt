6d138f626d97c879f86a2abd7461f3738ed8f0a6
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-10 20:35:31 +0900

    [feat/#84] WithDrawUiState 추가

--- app/src/main/java/com/moive/app/presentation/withdraw/WithDrawContract.kt
+++ app/src/main/java/com/moive/app/presentation/withdraw/WithDrawContract.kt
@@ -1,16 +1,25 @@
 package com.moive.app.presentation.withdraw
 
 import androidx.compose.runtime.Immutable
-import com.moive.app.core.designsystem.component.toast.ToastType
 
 interface WithDrawContract {
     @Immutable
     data class State(
         val isAgreed: Boolean = false,
         val showConfirmBottomSheet: Boolean = false,
+        val withDrawUiState: WithDrawUiState = WithDrawUiState.IDLE,
     )
 
     sealed class SideEffect {
         data object NavigateToLogin : SideEffect()
     }
+}
+
+sealed interface WithDrawUiState {
+    data object IDLE : WithDrawUiState
+    data object Loading : WithDrawUiState
+    data object Success : WithDrawUiState
+    data class Failure(
+        val msg: String,
+    ) : WithDrawUiState
 }
