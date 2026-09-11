c68d293fe909a5818fee312da8f93d2ae322a764
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-12 02:42:35 +0900

    [feat/#98] 모임 리스트 response, model, mapper 구현
    - 이미지 리스트 실제 값으로 수정 예정

--- app/src/main/java/com/moive/app/data/meeting/model/MeetingListModel.kt
+++ app/src/main/java/com/moive/app/data/meeting/model/MeetingListModel.kt
@@ -1,12 +1,21 @@
 package com.moive.app.data.meeting.model
 
+import androidx.compose.runtime.Immutable
 import com.moive.app.core.designsystem.component.chip.LabelType
 import kotlinx.collections.immutable.ImmutableList
 
+@Immutable
+data class MeetingListModel(
+    val meetings: ImmutableList<MeetingListCardItemModel>,
+    val hasNext: Boolean,
+    val nextCursor: Long?,
+)
+
+@Immutable
 data class MeetingListCardItemModel(
     val id: Long,
     val title: String,
-    val dateTime: String,
+    val dateTime: String?,
     val participantImageUrls: ImmutableList<String>,
     val statusText: String,
     val statusLabelType: LabelType,
