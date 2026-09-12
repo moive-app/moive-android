3e37982a313c360b4f59db6db4adba0dc6182d0a
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-12 21:37:34 +0900

    [feat/#105] 추천 장소 조회 repository/impl 추가

--- app/src/main/java/com/moive/app/data/voting/repository/VotingRepository.kt
+++ app/src/main/java/com/moive/app/data/voting/repository/VotingRepository.kt
@@ -1,7 +1,13 @@
 package com.moive.app.data.voting.repository
 
+import com.moive.app.data.voting.model.PlaceRecommendationCardItemModel
 import com.moive.app.data.voting.model.RegionPinModel
 
 interface VotingRepository {
     suspend fun getRecommendedAreas(meetingId: Long): Result<List<RegionPinModel>>
+
+    suspend fun getRecommendedPlaces(
+        meetingId: Long,
+        recommendedAreaId: Long,
+    ): Result<List<PlaceRecommendationCardItemModel>>
 }
--- app/src/main/java/com/moive/app/data/voting/repository/VotingRepositoryImpl.kt
+++ app/src/main/java/com/moive/app/data/voting/repository/VotingRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.moive.app.data.voting.repository
 import com.moive.app.core.utils.suspendRunCatching
 import com.moive.app.data.common.dto.checkData
 import com.moive.app.data.voting.mapper.toModel
+import com.moive.app.data.voting.model.PlaceRecommendationCardItemModel
 import com.moive.app.data.voting.model.RegionPinModel
 import com.moive.app.data.voting.remote.datasource.VotingRemoteDataSource
 import javax.inject.Inject
@@ -16,6 +17,17 @@ class VotingRepositoryImpl @Inject constructor(
             votingRemoteDataSource.getRecommendedAreas(meetingId)
                 .checkData()
                 .areas
+                .map { it.toModel() }
+        }
+
+    override suspend fun getRecommendedPlaces(
+        meetingId: Long,
+        recommendedAreaId: Long,
+    ): Result<List<PlaceRecommendationCardItemModel>> =
+        suspendRunCatching {
+            votingRemoteDataSource.getRecommendedPlaces(meetingId, recommendedAreaId)
+                .checkData()
+                .places
                 .map { it.toModel() }
         }
 }
