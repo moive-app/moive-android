6970b95a5b96fc7d1e083068b16dd4069acd1cda
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2026-09-13 02:26:17 +0900

    [feat/#109] 장소 투표 현황 repository/impl 추가

--- app/src/main/java/com/moive/app/data/votingstatus/repository/VoteStatusRepositoryImpl.kt
+++ app/src/main/java/com/moive/app/data/votingstatus/repository/VoteStatusRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.moive.app.data.votingstatus.repository
 import com.moive.app.core.utils.suspendRunCatching
 import com.moive.app.data.common.dto.checkData
 import com.moive.app.data.votingstatus.mapper.toModel
+import com.moive.app.data.votingstatus.model.PlaceVoteResultModel
 import com.moive.app.data.votingstatus.model.ScheduleVoteResultModel
 import com.moive.app.data.votingstatus.remote.datasource.VoteStatusRemoteDataSource
 import javax.inject.Inject
@@ -14,6 +15,13 @@ class VoteStatusRepositoryImpl @Inject constructor(
     override suspend fun getScheduleVoteResult(meetingId: Long): Result<ScheduleVoteResultModel> =
         suspendRunCatching {
             voteStatusRemoteDataSource.getScheduleVoteResult(meetingId)
+                .checkData()
+                .toModel()
+        }
+
+    override suspend fun getPlaceVoteResult(meetingId: Long): Result<PlaceVoteResultModel> =
+        suspendRunCatching {
+            voteStatusRemoteDataSource.getPlaceVoteResult(meetingId)
                 .checkData()
                 .toModel()
         }
